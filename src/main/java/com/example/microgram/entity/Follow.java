7829abe0ce00408bd9909c33f3a8773af7404fa6
Begaiym Kalilova <easybeka@gmail.com>
2023-03-18 02:51:45 +0600

    added required fields

--- src/main/java/com/example/microgram/entity/Follow.java
+++ src/main/java/com/example/microgram/entity/Follow.java
@@ -2,9 +2,12 @@ package com.example.microgram.entity;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class Follow {
     private User userBeingFollowed;
     private User userFollowing;
-    private String followDate;
+    private LocalDate followDate;
+    private Long countFollowers;
 }
